f6a7c7e64d07b0b062f228d688c35e89cb385a5e
Nico <nicovigil1@gmail.com>
2019-01-27 16:32:08 -0700

    adds test for customer search endpoints

--- spec/requests/api/v1/customer_spec.rb
+++ spec/requests/api/v1/customer_spec.rb
@@ -18,5 +18,45 @@ describe "Customer API" do
 
         expect(response).to be_successful
         expect(JSON.parse(response.body)["first_name"]).to eq(customer.first_name) 
+    end
+    
+    describe 'can find one or all by parameters' do
+        it 'can find a merchant by id' do 
+            customer = create(:customer)
+
+            get "/api/v1/customers/find?id=#{customer.id}"
+
+            expect(response).to be_successful
+            expect(JSON.parse(response.body)["data"]["id"]).to eq("#{customer.id}") 
+        end
+        it 'can find a merchant by first name' do 
+            customer = create(:customer, first_name: "how now", last_name: "brown cow")
+            customer1 = create(:customer, first_name: "how now", last_name: "brown cow")
+
+            get "/api/v1/customers/find?first_name=#{customer.first_name}"
+
+            expect(response).to be_successful
+            expect(JSON.parse(response.body)["data"]["id"]).to eq("#{customer.id}")
+
+            
+            get "/api/v1/customers/find_all?name=#{customer.first_name}"
+
+            expect(response).to be_successful
+            expect(JSON.parse(response.body)["data"].length).to eq(2)
+        end
+        it 'can find a merchant by last name' do 
+            customer = create(:customer, last_name: "how now")
+            customer1 = create(:customer, last_name: "how now")
+
+            get "/api/v1/customers/find?last_name=#{customer.last_name}"
+
+            expect(response).to be_successful
+            expect(JSON.parse(response.body)["data"]["id"]).to eq("#{customer.id}")
+
+            get "/api/v1/customers/find_all?last_name=#{customer.last_name}"
+
+            expect(response).to be_successful
+            expect(JSON.parse(response.body)["data"].length).to eq(2)
+        end 
     end 
 end
