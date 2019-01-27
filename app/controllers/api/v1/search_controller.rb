b687ea0ff46017f3b7143e7dcdd61f78368515e3
Nico <nicovigil1@gmail.com>
2019-01-27 16:34:57 -0700

    adds find endpoint for customers

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,14 +2,24 @@ class Api::V1::SearchController < ApplicationController
     def find_merchant
         render json: MerchantSerializer.new(Merchant.find_by(merchant_params))
     end
-
     def find_merchants
         render json: MerchantSerializer.new(Merchant.where(merchant_params))
+    end
+
+    def find_customer
+        render json: CustomerSerializer.new(Customer.find_by(customer_params))
+    end 
+    def find_customers
+        render json: CustomerSerializer.new(Customer.where(customer_params))
     end 
 
     private
-
+    
     def merchant_params
         params.permit(:id, :name, :created_at, :updated_at)
+    end
+    
+    def customer_params
+        customer = params.permit(:first_name, :last_name)
     end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,28 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do
-      scope :merchants do 
-        get '/find', to: "search#find_merchant"
-        get '/find_all', to: "search#find_merchants"
-      end 
+
+      scopes = ["merchant", "customer"]
+
+      scopes.each do |resource|
+        scope resource.pluralize.to_sym do 
+          get 'find', to: "search#find_#{resource}"
+          get 'find_all', to: "search#find_#{resource.pluralize}"
+        end
+      end
+
+      # scope :merchants do 
+      #   get '/find', to: "search#find_merchant"
+      #   get '/find_all', to: "search#find_merchants"
+      # end 
       resources :merchants, only: [:index, :show]
 
+      # scope :customers do 
+      #   get '/find', to: "search#find_customer"
+      #   get '/find_all', to: "search#find_customers"
+      # end 
       resources :customers, only: [:index, :show]
+
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
