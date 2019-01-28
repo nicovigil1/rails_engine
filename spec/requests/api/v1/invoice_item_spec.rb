023254c96d10682f5c77e0981ac1483f9036884d
Nico <nicovigil1@gmail.com>
2019-01-27 17:32:14 -0700

    adds test for finding invoice items by attributes

--- spec/requests/api/v1/invoice_item_spec.rb
+++ spec/requests/api/v1/invoice_item_spec.rb
@@ -24,7 +24,60 @@ describe "invoice_item api" do
         get "/api/v1/invoice_items/#{invoice_item.id}"
 
         expect(response).to be_successful 
-        expect(JSON.parse(response.body)["item_id"]).to eq(item.id) 
-
+        expect(JSON.parse(response.body)["id"]).to eq(invoice_item.id) 
     end 
+
+    describe "can find invoice_item by the attribute:" do
+        it 'id' do 
+            merchant = create(:merchant)
+            customer = create(:customer)
+            item = create(:item, merchant: merchant)
+            invoice = create(:invoice, merchant: merchant, customer: customer)
+            invoice_item = create(:invoice_item, invoice: invoice, item: item )
+
+            get "/api/v1/invoice_items/find?id=#{invoice_item.id}"
+
+            expect(response).to be_successful 
+            expect(JSON.parse(response.body)["data"]["id"]).to eq(invoice_item.id.to_s) 
+        end 
+        
+        it 'quantity' do
+            merchant = create(:merchant)
+            customer = create(:customer)
+            item = create(:item, merchant: merchant)
+            invoice = create(:invoice, merchant: merchant, customer: customer)
+            invoice_item = create(:invoice_item, invoice: invoice, item: item, quantity: 12 )
+            invoice_item1 = create(:invoice_item, invoice: invoice, item: item, quantity: 12 )
+
+            get "/api/v1/invoice_items/find?quantity=#{invoice_item.quantity}"
+
+            expect(response).to be_successful 
+            expect(JSON.parse(response.body)["data"]["id"]).to eq(invoice_item.id.to_s) 
+
+            get "/api/v1/invoice_items/find_all?quantity=#{invoice_item.quantity}"
+
+            expect(response).to be_successful 
+            expect(JSON.parse(response.body)["data"].length).to eq(2) 
+        end 
+
+        it 'unit_price' do 
+            merchant = create(:merchant)
+            customer = create(:customer)
+            item = create(:item, merchant: merchant)
+            invoice = create(:invoice, merchant: merchant, customer: customer)
+            invoice_item = create(:invoice_item, invoice: invoice, item: item, unit_price: 12 )
+            invoice_item1 = create(:invoice_item, invoice: invoice, item: item, unit_price: 12 )
+
+            get "/api/v1/invoice_items/find?unit_price=#{invoice_item.unit_price.to_s}"
+
+            expect(response).to be_successful 
+            expect(JSON.parse(response.body)["data"]["id"]).to eq(invoice_item.id.to_s) 
+
+            get "/api/v1/invoice_items/find_all?unit_price=#{invoice_item.unit_price.to_s}"
+
+            expect(response).to be_successful 
+            expect(JSON.parse(response.body)["data"].length).to eq(2)
+        end 
+    end
+    
 end
